5a495f884c1e30b09d86d5f94ea3cf9ccfbbd4f6
NicholeJoyce <santosng@students.nu-baliwag.edu.ph>
2023-04-16 21:37:47 +0800

    changed the double quotes to single quote

--- send.php
+++ send.php
@@ -6,17 +6,17 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
  
-if(isset($_POST["send"])){
+if(isset($_POST['send'])){
     $name = $_POST['name'];
     $message = "Hi, ". $name . "! This is to acknowledge your email that we have receieved it, we'll get back to you as soon as we can. Regards, RWS Trucking Services!";
     
     $mail = new PHPMailer(true);
     $mail->isSMTP();
-    $mail->Host = "smtp.hostinger.com";
+    $mail->Host = 'smtp.hostinger.com';
     $mail->SMTPAuth = 'true';
-    $mail->Username = "contact@dnails.shop";
-    $mail->Password = "Nichole@15";
-    $mail->SMTPSecure = "tls";
+    $mail->Username = 'contact@dnails.shop';
+    $mail->Password = 'Nichole@15';
+    $mail->SMTPSecure = 'tls';
     $mail->Port = '587';
 
     $mail->setFrom('contact@dnails.shop', 'RWS Trucking Services');
@@ -25,7 +25,7 @@ if(isset($_POST["send"])){
     $email = $_POST['email'];
     $mail->addAddress($email);
     $mail->isHTML(true);
-    $mail->Subject = $_POST["subject"];
+    $mail->Subject = $_POST['subject'];
     $mail->Body =($message);
     $mail->send();
     echo "Feedback Successful!";}
