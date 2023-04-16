7d7ea22230b7e4b5721a6163de72e7abbd3e8862
NicholeJoyce <santosng@students.nu-baliwag.edu.ph>
2023-04-16 22:15:45 +0800

    edited the auto-generated message

--- send.php
+++ send.php
@@ -60,7 +60,7 @@ if(isset($_POST["send"])){
     $mail->addAddress($email); //Receivers Email
     $mail->isHTML(true);
     $mail->Subject = "Good Day!";
-    $mail->Body = "Thank you for contacting us! We will get back to you as soon as we can. Have a wonderful day " . $name;
+    $mail->Body = "Thank you for your message, " . $name . " I appreciate you taking the time to reach out to me. Please know that I have received your message, and I will do my best to respond as soon as possible. Your patience and understanding are greatly appreciated, and I look forward to getting back to you soon. Once again, thank you for your message, and have a wonderful day. Regards, RWS Trucking Services";
     $mail->send();
     echo "Feedback Successful!";
 }
