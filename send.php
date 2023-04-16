d1e14b7e87ff4a2206d900bf6f91350073a27b1b
NicholeJoyce <santosng@students.nu-baliwag.edu.ph>
2023-04-16 21:09:02 +0800

    edited the email chennelyn

--- send.php
+++ send.php
@@ -1,40 +1,31 @@
-<?php 
+<?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
-
-
+ 
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+ 
 if(isset($_POST["send"])){
+    $name = $_POST['name'];
+    $message = "Hi, ". $name . "! This is to acknowledge your email that we have receieved it, we'll get back to you as soon as we can. Regards, RWS Trucking Services!";
     $mail = new PHPMailer(true);
-
     $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'nicholejoycesantos1122@gmail.com';
-    $mail->Password = 'xmzsynlcjtfdssly';
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port = 465;
+    $mail->Host = "smtp.hostinger.com";
+    $mail->SMTPAuth = 'true';
+    $mail->Username = "contact@dnails.shop";
+    $mail->Password = "Nichole@15";
+    $mail->SMTPSecure = "tls";
+    $mail->Port = 587;
 
-    $mail->setFrom('nicholejoycesantos1122@gmail.com');
-    $mail->addAddress('nicholejoycesanto1122@gmail.com');
+    $mail->setFrom('contact@dnails.shop', 'RWS Trucking Services');
+    
     $mail->isHTML(true);
-
+    $email = $_POST['email'];
+    $mail->addAddress($email);
+    $mail->isHTML(true);
     $mail->Subject = $_POST["subject"];
-    $mail->Body = $_POST["message"];
-
+    $mail->Body =($message);
     $mail->send();
-
-    echo 
-    "
-    <script>
-    alert('Sent Successfully!');
-    document.location.href='contact.php';
-    </script>
-    ";
-
-}
-
+    echo "Feedback Successful!";}
 ?>
