51b4d8bff90d6cfb90b2ce428ec3a375de4fdb01
NicholeJoyce <santosng@students.nu-baliwag.edu.ph>
2023-04-22 22:27:14 +0800

    it's here

--- send.php
+++ send.php
@@ -60,8 +60,8 @@ if(isset($_POST["submit"])){
     $mail->setFrom('contact@dnails.shop', 'RWS Trucking Services'); //Senders Email
     $mail->addAddress($email); //Receivers Email
     $mail->isHTML(true);
-    $mail->Subject = "Good Day!";
-    $mail->Body = "<hr>" . "Thank you for your message " ."<b>$name</b>" . "! " . "<br> I appreciate you taking the time to reach out to us. Please know that I have received your message, and I will do my best to respond as soon as possible. Your patience and understanding are greatly appreciated, and I look forward to getting back to you soon. Once again, thank you for your message, and have a wonderful day. 
+    $mail->Subject = "RWS Trucking Services";
+    $mail->Body = "<hr>" . "Good Day " ."<b>$name</b>" . "! " . "<br> I appreciate you taking the time to reach out to us. Please know that I have received your message, and I will do my best to respond as soon as possible. Your patience and understanding are greatly appreciated, and I look forward to getting back to you soon. Thank you for your message, and have a wonderful day. 
      <br><br>        
           Regards, <br>
          <b>RWS Trucking Service </b>";
