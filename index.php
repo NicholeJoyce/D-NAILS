1fbe5054c38b141e3ae755b0778f84457472c559
NicholeJoyce <santosng@students.nu-baliwag.edu.ph>
2023-04-22 18:50:12 +0800

    lipatbahay

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
           <li class="dropdown"><a href="#"><span><b>Services</b></span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
             <li><a href="haul.html">Haul</a></li>
-              <li><a href="services.html">Lipat Bahay</a></li>
+              <li><a href="lipatbahay.html">Lipat Bahay</a></li>
               <li><a href="services.html">Office Transfer</a></li>
               <li><a href="services.html">Raw Materials Delivery</a></li>
               <li><a href="services.html">Warehouse Transfer</a></li>
