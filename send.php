5d152ad95b07a623e39d1478a5b7a05d1cd79350
NicholeJoyce <santosng@students.nu-baliwag.edu.ph>
2023-04-22 21:21:36 +0800

    what the

--- send.php
+++ send.php
@@ -65,10 +65,10 @@
 //     $mail->send();
 //    
 
-echo "Message Sent Successfully!";
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
+echo "Message Sent Successfully!";
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
